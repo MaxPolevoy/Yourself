87b9df29349917d2d9bfd617d2d3f0d631b8acda
MaxPolevoy <Lidiya-1958>
2022-05-20 15:46:08 +0300

    Two Array

---
+++ src/com/company/Array/ItprogerСom/TwoArray.java
@@ -0,0 +1,13 @@
+package com.company.Array.ItprogerСom;
+
+public class TwoArray {
+    public static void main(String[] args) {
+        int[][] arr = new int[][]{
+                {1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9}
+        };
+        arr [1][2] = 10;
+        System.out.println(arr[1][2]);
+    }
+}
