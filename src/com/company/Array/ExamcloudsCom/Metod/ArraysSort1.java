71326ded531af6f697fcd55e869c822f054c8816
MaxPolevoy <Lidiya-1958>
2022-05-23 16:57:10 +0300

    отсортировать двухмерный массив

---
+++ src/com/company/Array/ExamcloudsCom/Metod/ArraysSort1.java
@@ -0,0 +1,14 @@
+package com.company.Array.ExamcloudsCom.Metod;
+
+//Сортировка массива - это частая задача при написании Java приложений.
+// Метод Arrays.sort() позволяет сортировать элементы массива по возрастанию:
+
+import java.util.Arrays;
+
+public class ArraysSort1 {
+    public static void main(String[] args) {
+        int[] array = new int[]{3, 1, 5, 6, 8};
+        Arrays.sort(array);
+        System.out.println(Arrays.toString(array));
+    }
+}
