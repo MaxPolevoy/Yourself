bdf1f2d602c26303622f757591cf5341996f33de
MaxPolevoy <Lidiya-1958>
2022-05-23 19:07:55 +0300

    Задание 5 Найти максимальное и минимальное значение в каждой "строке" и записать эти значения в двухмерный массив 5х2.

---
+++ src/com/company/Array/ExamcloudsCom/Metod/job_05.java
@@ -0,0 +1,42 @@
+package com.company.Array.ExamcloudsCom.Metod;
+
+//Создать двумерный массив 5х8 типа int и инициализировать его с помощью блока для инициализации.
+//Найти максимальное и минимальное значение в каждой "строке" и записать эти значения в двухмерный массив 5х2.
+//Распечатать массив, содержащий максимальное и минимальное значение.
+
+public class job_05 {
+    public static void main(String[] args) {
+        int[][] array2 = new int[5][2];
+        int[][] array = {
+                {8, 2, 3, 8, 5, 6, 7, 4},
+                {3, 3, 4, 5, 9, 7, 8, 6},
+                {10, 4, 5, 6, 7, 8, 9, 3},
+                {4, 5, 6, 7, 8, 9, 10, 11},
+                {5, 6, 7, 8, 9, 10, 11, 12},
+        };
+        int max = array[0][0];
+        int min = array[0][0];
+
+        for (int[] row : array) {
+            for (int col : row) {
+                if (max < col) {
+                    max = col;
+                }
+            }
+            System.out.print(max);
+            System.out.println();
+        }
+        System.out.println();
+        for (int i = 0; i < array.length; i++) {
+            if (min > array[i][0]) {
+                min = array[i][0];
+            }
+
+        }System.out.print(min);
+        System.out.println();
+
+
+    }
+}
+
+
